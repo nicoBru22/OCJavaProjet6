dbfa9c0960207b77084e01c05c09a20e241d347b
nicoBru22 <brunet.nicolas35@gmail.com>
2025-01-24 13:05:50 +0100

    test pour transactionCOntroller réalisé. coverage à 70%

--- src/main/java/com/projet6/payMyBuddy/controller/TransactionController.java
+++ src/main/java/com/projet6/payMyBuddy/controller/TransactionController.java
@@ -3,6 +3,8 @@ package com.projet6.payMyBuddy.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,11 +20,13 @@ public class TransactionController {
 	private TransactionService transactionService;
 
 	@PostMapping("/add_transaction")
-	public String addTransaction(@RequestParam String email, @RequestParam String description, @RequestParam double amount) throws Exception {
+	public ResponseEntity<?> addTransaction(@RequestParam String email, @RequestParam String description, @RequestParam double amount) throws Exception {
 		logger.info("Entrée dans la méthode addTransaction de la class TransactionCOntroller.");
 		logger.debug("les données en paramètre. email : {}, description :{}, amount : {} ", email, description, amount);
-			transactionService.addTransaction(email, description, amount);
-			return "redirect:/transfer";
+		
+		transactionService.addTransaction(email, description, amount);
+			
+		return ResponseEntity.status(HttpStatus.CREATED).header("location", "/transfer").build();
 
 	}
 	
---
+++ src/test/java/com/projet6/payMyBuddy/controller/TransactionControllerTest.java
@@ -0,0 +1,55 @@
+package com.projet6.payMyBuddy.controller;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.projet6.payMyBuddy.model.Transactions;
+import com.projet6.payMyBuddy.service.TransactionService;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TransactionControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private TransactionService transactionService;
+	
+	@Test
+    @WithMockUser(username = "testUser", roles = "USER")
+	void testAddTransaction() throws Exception {
+		
+		String emailTest = "email@test.fr";
+		String descriptionTest = "test description de la transaction";
+		double amountTest = 12.34;
+		
+		Transactions transactionTest = new Transactions();
+		transactionTest.setId(1L);
+		transactionTest.setAmount(amountTest);
+		transactionTest.setDescription(descriptionTest);
+		transactionTest.setReceiver(null);
+		transactionTest.setSender(null);
+		
+		when(transactionService.addTransaction(emailTest, descriptionTest, amountTest)).thenReturn(transactionTest);
+		
+		mockMvc.perform(post("/add_transaction")
+				.param("email", emailTest)
+				.param("description", descriptionTest)
+		        .param("amount", String.valueOf(amountTest)))
+			.andExpect(status().isCreated())
+			.andExpect(header().string("location", "/transfer"));
+	}
+
+}
