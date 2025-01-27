a9bb366d74bb10a318a19bb691271d49a3e522c1
nicoBru22 <brunet.nicolas35@gmail.com>
2025-01-27 20:01:13 +0100

    Javadoc faite pour UserService et TransactionService.

--- src/main/java/com/projet6/payMyBuddy/service/TransactionService.java
+++ src/main/java/com/projet6/payMyBuddy/service/TransactionService.java
@@ -12,74 +12,97 @@ import com.projet6.payMyBuddy.model.User;
 import com.projet6.payMyBuddy.repository.TransactionRepository;
 import com.projet6.payMyBuddy.repository.UserRepository;
 
+/**
+ * Service pour gérer les transactions entre utilisateurs.
+ * Fournit des méthodes pour récupérer, ajouter et gérer les transactions.
+ */
 @Service
 public class TransactionService {
-	
-	private Logger logger = LogManager.getLogger(TransactionService.class);
-	
-	@Autowired
-	private TransactionRepository transactionRepository;
-	
-	@Autowired
-	private UserService userService;
-	
-	@Autowired
-	private UserRepository userRepository;
 
-	public List<Transactions> getAllTransactions() throws Exception {
-		logger.info("Entrée dans la méthode getAllTransactions() de la class TransactionService.");
-		try {
-			List<Transactions> transactionsList = transactionRepository.findAll();
-			logger.debug("La liste des transactions : {}", transactionsList);
-			return transactionsList;
-		} catch (Exception e) {
-			logger.error("Une erreur est survenue lors de la récupération de toutes les transacations." + e);
-			throw new Exception("Une erreur est survenue lors de la récupération de toutes les transacations.");
-		}
-	}
-	
-	public Transactions addTransaction(String email, String description, Double amount) throws Exception {
-		logger.info("Entrée dans la méthode addTransaction de la class TransactionService.");
-		logger.debug("les données en paramètre. email : {}, description :{}, amount : {} ", email, description, amount);
-		try {
-			User sender = userService.getCurrentUser();
-			User receiver = userRepository.findByEmail(email);
-			
-			logger.debug("Le sender : {} ", sender);
-			logger.debug("Le receiver : {} ", receiver);
-			
-			if (receiver == null) {
-			    logger.error("Le destinataire avec l'email {} n'existe pas.", email);
-			    throw new Exception("Destinataire introuvable.");
-			} else {
-				Transactions newTransaction = new Transactions();
-				newTransaction.setSender(sender);
-				newTransaction.setReceiver(receiver);
-				newTransaction.setDescription(description);
-				newTransaction.setAmount(amount);
-				
-				logger.debug("La nouvelle transaction à sauvegarder : {} ", newTransaction);
-				return transactionRepository.save(newTransaction);
-			}
-		} catch (Exception e) {
-			logger.error("Une erreur est survenue lors de l'ajout d'une nouvelle transacation.");
-			throw new Exception("Une erreur est survenue lors de l'ajout d'une nouvelle transacation", e);
-		}
-	}
-	
-	public List<Transactions> getAllTransactionById() throws Exception {
-		logger.info("Entrée dans la méthode TransationService.getAllTransactionById.");
-		try {
-			User user = userService.getCurrentUser();
-			logger.debug("Le currentUser est : {} ", user);
-			List<Transactions> transactionList = transactionRepository.findBySender(user);
-			logger.debug("la liste es transactions : {} ", transactionList);
-			return transactionList;
-		} catch (Exception e) {
-			logger.error("\"Une erreur est survenue lors de la récupération des transactions.");
-			throw new Exception("Une erreur est survenue lors de la récupération des transactions");
-		}
-	}
-	
-	
+    private Logger logger = LogManager.getLogger(TransactionService.class);
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    /**
+     * Récupère toutes les transactions disponibles dans la base de données.
+     *
+     * @return une liste de toutes les transactions.
+     * @throws Exception si une erreur survient lors de la récupération des transactions.
+     */
+    public List<Transactions> getAllTransactions() throws Exception {
+        logger.info("Entrée dans la méthode getAllTransactions() de la classe TransactionService.");
+        try {
+            List<Transactions> transactionsList = transactionRepository.findAll();
+            logger.debug("La liste des transactions : {}", transactionsList);
+            return transactionsList;
+        } catch (Exception e) {
+            logger.error("Une erreur est survenue lors de la récupération de toutes les transactions.", e);
+            throw new Exception("Une erreur est survenue lors de la récupération de toutes les transactions.");
+        }
+    }
+
+    /**
+     * Ajoute une nouvelle transaction entre l'utilisateur courant et un destinataire.
+     *
+     * @param email l'email du destinataire.
+     * @param description une description de la transaction.
+     * @param amount le montant de la transaction.
+     * @return la transaction créée et sauvegardée.
+     * @throws Exception si une erreur survient lors de l'ajout de la transaction ou si le destinataire est introuvable.
+     */
+    public Transactions addTransaction(String email, String description, Double amount) throws Exception {
+        logger.info("Entrée dans la méthode addTransaction de la classe TransactionService.");
+        logger.debug("Les données en paramètre : email : {}, description : {}, amount : {}", email, description, amount);
+        try {
+            User sender = userService.getCurrentUser();
+            User receiver = userRepository.findByEmail(email);
+
+            logger.debug("L'utilisateur émetteur (sender) : {}", sender);
+            logger.debug("L'utilisateur destinataire (receiver) : {}", receiver);
+
+            if (receiver == null) {
+                logger.error("Le destinataire avec l'email {} n'existe pas.", email);
+                throw new Exception("Destinataire introuvable.");
+            } else {
+                Transactions newTransaction = new Transactions();
+                newTransaction.setSender(sender);
+                newTransaction.setReceiver(receiver);
+                newTransaction.setDescription(description);
+                newTransaction.setAmount(amount);
+
+                logger.debug("La nouvelle transaction à sauvegarder : {}", newTransaction);
+                return transactionRepository.save(newTransaction);
+            }
+        } catch (Exception e) {
+            logger.error("Une erreur est survenue lors de l'ajout d'une nouvelle transaction.", e);
+            throw new Exception("Une erreur est survenue lors de l'ajout d'une nouvelle transaction.", e);
+        }
+    }
+
+    /**
+     * Récupère toutes les transactions associées à l'utilisateur actuellement connecté.
+     *
+     * @return une liste de transactions dont l'utilisateur courant est l'émetteur.
+     * @throws Exception si une erreur survient lors de la récupération des transactions.
+     */
+    public List<Transactions> getAllTransactionById() throws Exception {
+        logger.info("Entrée dans la méthode getAllTransactionById() de la classe TransactionService.");
+        try {
+            User user = userService.getCurrentUser();
+            logger.debug("L'utilisateur courant est : {}", user);
+            List<Transactions> transactionList = transactionRepository.findBySender(user);
+            logger.debug("La liste des transactions : {}", transactionList);
+            return transactionList;
+        } catch (Exception e) {
+            logger.error("Une erreur est survenue lors de la récupération des transactions.", e);
+            throw new Exception("Une erreur est survenue lors de la récupération des transactions.", e);
+        }
+    }
 }
--- src/main/java/com/projet6/payMyBuddy/service/UserService.java
+++ src/main/java/com/projet6/payMyBuddy/service/UserService.java
@@ -15,6 +15,10 @@ import org.springframework.stereotype.Service;
 import com.projet6.payMyBuddy.model.User;
 import com.projet6.payMyBuddy.repository.UserRepository;
 
+/**
+ * Service pour gérer les utilisateurs. Fournit des méthodes pour récupérer,
+ * ajouter et gérer les utilisateurs.
+ */
 @Service
 public class UserService {
 
@@ -22,6 +26,26 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	/**
+	 * Récupère l'utilisateur actuellement authentifié, en prenant en charge
+	 * l'authentification OAuth2 ou classique.
+	 * 
+	 * <p>
+	 * Cette méthode utilise le contexte de sécurité pour accéder à
+	 * l'authentification de l'utilisateur connecté. Si l'utilisateur est
+	 * authentifié via OAuth2, ses informations sont récupérées à l'aide de la
+	 * méthode {@link #getUserWithOauth(OAuth2User)}. Pour une authentification
+	 * classique, les détails d'utilisateur sont traités via
+	 * {@link #getUserWithUserDetails()}.
+	 * </p>
+	 * 
+	 * @return un objet {@link User} représentant l'utilisateur actuellement
+	 *         authentifié, ou {@code null} si l'utilisateur ne peut pas être
+	 *         déterminé
+	 * @throws Exception si une erreur survient lors de la récupération de
+	 *                   l'utilisateur
+	 */
 
 	public User getCurrentUser() throws Exception {
 		logger.info("Entrée dans la méthode UserService.getcurrentUser().");
@@ -34,7 +58,7 @@ public class UserService {
 			if (principal instanceof OAuth2User) { // Vérifiez que l'utilisateur est authentifié via OAuth2
 				User oAuthUser = getUserWithOauth((OAuth2User) principal); // Cast explicite
 				return oAuthUser;
- 
+
 			} else if (principal instanceof UserDetails) {
 				logger.debug("Utilisateur authentifié sur le site.");
 				User currentUser = getUserWithUserDetails();
@@ -54,6 +78,24 @@ public class UserService {
 		return null;
 	}
 
+	/**
+	 * Récupère l'utilisateur actuellement authentifié via les détails
+	 * d'authentification du contexte de sécurité.
+	 * 
+	 * <p>
+	 * Cette méthode accède au contexte de sécurité pour obtenir l'objet principal
+	 * de l'utilisateur connecté. Si le principal est une instance de
+	 * {@link UserDetails}, l'email est extrait et utilisé pour récupérer
+	 * l'utilisateur correspondant depuis le repository. Si le principal n'est pas
+	 * une instance de {@link UserDetails}, un avertissement est enregistré et la
+	 * méthode retourne {@code null}.
+	 * </p>
+	 * 
+	 * @return un objet {@link User} représentant l'utilisateur actuellement
+	 *         authentifié, ou {@code null} si l'utilisateur ne peut pas être
+	 *         déterminé
+	 */
+
 	public User getUserWithUserDetails() {
 		logger.info("Entrée dans la méthode userService.getUserWithUserDetails.");
 		try {
@@ -72,6 +114,23 @@ public class UserService {
 			return null;
 		}
 	}
+
+	/**
+	 * Récupère ou crée un utilisateur en fonction des données fournies par OAuth2.
+	 * 
+	 * <p>
+	 * Cette méthode tente de récupérer un utilisateur à partir de son adresse email
+	 * via le repository. Si aucun utilisateur correspondant n'est trouvé, un nouvel
+	 * utilisateur est créé et ajouté au système. La méthode gère également les
+	 * exceptions en cas de problème lors de l'exécution.
+	 * </p>
+	 * 
+	 * @param oAuth2User l'utilisateur OAuth2 contenant les informations de
+	 *                   connexion telles que le login et l'email
+	 * @return un objet {@link User} représentant l'utilisateur récupéré ou créé
+	 * @throws Exception si une erreur se produit lors de la récupération ou de la
+	 *                   création de l'utilisateur
+	 */
 
 	public User getUserWithOauth(OAuth2User oAuth2User) throws Exception {
 		logger.info("Entrée dans la méthode UserService.getUserWithOAuth().");
@@ -99,6 +158,18 @@ public class UserService {
 		}
 	}
 
+	/**
+	 * Récupère la liste de tous les utilisateurs.
+	 * 
+	 * <p>
+	 * La méthode récupère la liste contenant tous les utilisateurs et retourne
+	 * cette liste.
+	 * </p>
+	 * 
+	 * @return la liste des utilisateurs.
+	 * @throws Exception si une erreur survient lors de la récupération de la liste
+	 *                   des utilisateurs.
+	 */
 	public List<User> getAllUser() throws Exception {
 		logger.info("Entreé dans la méthode userService.getAllUser().");
 		try {
@@ -112,6 +183,21 @@ public class UserService {
 
 	}
 
+	/**
+	 * Ajoute une nouvel connexion entre utilisateur.
+	 * 
+	 * <p>
+	 * La méthode récupère l'adresse email de l'utilisateur à ajouter en paramètre.
+	 * Elle récupère les données sur l'utilisateur actuel puis celle de
+	 * l'utilisateur à qui appartient l'adresse email. Si l'utilisateur à ajouter
+	 * est nul alors elle lève une exception sinon elle sauvegarde la connexion du
+	 * coté des 2 utilisateurs.
+	 * </p>
+	 * 
+	 * @param email l'email de l'utilisateur à ajouter.
+	 * @throws Exception si un utilisateur n'est pas trouvé via l'email ou si une
+	 *                   erreur se produit pendant l'ajout.
+	 */
 	public void addConnection(String email) throws Exception {
 		try {
 			logger.debug("l'email dans le service = " + email);
@@ -141,6 +227,23 @@ public class UserService {
 
 	}
 
+	/**
+	 * Ajoute un nouvel utilisateur à la base de donnée.
+	 * 
+	 * <p>
+	 * La méthode récupère le nom, l'email et le mot de passe dans les paramètres.
+	 * Ensuite le mot de passe est hashé. Pour finir, elle créé un nouvel
+	 * utilisateur avec l'ensemble des éléments et retoune la sauvegarde du nouvel
+	 * utilisateur.
+	 * </p>
+	 * 
+	 * @param username le nom du nouvel utilisateur
+	 * @param email    l'email du nouvel utilisateur.
+	 * @param password le mot de passe du nouvel utilisateur.
+	 * @return la sauvegarde du nouvel utilisateur.
+	 * @throws Exception si un problème a eu lieu lors de l'ajout du nouvel
+	 *                   utilisateur.
+	 */
 	public User addUser(String username, String email, String password) throws Exception {
 		logger.debug("Entrée dans la méthode addUser de la class UserService");
 		try {
@@ -162,6 +265,20 @@ public class UserService {
 
 	}
 
+	/**
+	 * Récupère la liste des connexions aux autres utilisateur.
+	 * 
+	 * <p>
+	 * La méthode récupère les données de l'utilisateur connecté actuellement. Si
+	 * l'utilisateur est null alors une exception est levée. Si l'utilisateur n'est
+	 * pas null, alors la méthode retourne la liste de connexion.
+	 * </p>
+	 * 
+	 * @return une liste des connexions à d'autres utilisateurs, à partir de
+	 *         l'utilisateur connecté.
+	 * @throws Exception une exception est levée si une erreur survient lors de la
+	 *                   récupération des connexions ou si l'utilisateur est null.
+	 */
 	public List<User> getConnections() throws Exception {
 		logger.info("Entrée dans la méthode UserService.getConnections().");
 		User currentUser = null;
